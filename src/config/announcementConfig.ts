e456ec682ed5bc93050f9bb8040040867ec4665b
Furjesk <w774467672@163.com>
2026-09-17 18:52:52 +0800

    feat: 修改网站上线日期

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -4,15 +4,15 @@ export const announcementConfig: AnnouncementConfig = {
 	// 公告标题，留空则走i18n默认标题
 	title: "",
 
-	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	// 公告内容 ✈️ 🚀
+	content: "SpringBoot 教程即将上线✈️",
 
 	// 是否允许用户关闭公告
 	closable: true,
 
 	link: {
 		// 启用链接
-		enable: true,
+		enable: false,
 		// 链接文本
 		text: "了解更多",
 		// 链接 URL
