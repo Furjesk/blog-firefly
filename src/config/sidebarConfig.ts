787b04634b8447e29b5ccbfcbbad03844eaf4298
Furjesk <w774467672@163.com>
2026-09-18 20:11:55 +0800

    fix: 修复支持博主链接跳转

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -225,7 +225,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 						"如果您觉得本站内容对您有帮助，欢迎支持我们的创作！您的支持是我们持续更新的动力。",
 					link: {
 						text: "支持一下",
-						url: "about/",
+						url: "/sponsor/",
 						external: false,
 					},
 					closable: false,
